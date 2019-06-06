c0f65b26896abe18d4d689968cf1027fac3b0110
lloan <lloanalas@outlook.com>
2019-06-05 22:29:48 -0700

    moved port, reformat

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  request = require("request");
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    request = require('request');
 
-// Creates express app
 const app = express();
-// The port used for Express server
-const PORT = 1428;
 
 // Starts server
-app.listen(process.env.PORT || PORT, function () {
-  console.log("Bot is listening on port " + PORT);
+app.listen(process.env.PORT, () => {
+  console.log('Bot is listening on port ' + PORT);
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post("/", (req, res) => {
-  var data = {
+app.post('/', (req, res) => {
+  let data = {
     form: {
       token: process.env.SLACK_AUTH_TOKEN,
-      channel: "#bot",
-      text: "Hi! :wave: \n I'm your new bot."
-    }
+      channel: '#bot',
+      text: 'Hi! :wave: \n I\'m your new bot.',
+    },
   };
-  request.post("https://slack.com/api/chat.postMessage", data, function (error, response, body) {
-    // Sends welcome message
-    res.json();
+
+  // post request to slack API
+  request.post('https://slack.com/api/chat.postMessage', data, (error, response, body) => {
+    res.json(); // sends message
   });
 });

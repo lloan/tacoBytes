ecd83de0bebc2b4ee00aa97bb328507781b8d2b8
lloan <lloanalas@outlook.com>
2019-06-06 14:45:50 -0700

    reformat, broken out to functions, check if self assigning tacos, promise based now

--- index.js
+++ index.js
@@ -1,108 +1,172 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    request = require('request'),
-    redis = require('redis');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  request = require("request"),
+  redis = require("redis");
 
 const client = redis.createClient();
 const app = express();
 
 // Starts server
 app.listen(process.env.PORT, () => {
-  console.log('Bot is listening on port ' + process.env.PORT);
+  console.log("Bot is listening on port " + process.env.PORT);
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-client.on('connect', function() {
-  console.log('connected');
+client.on("connect", function () {
+  console.log("connected");
 });
 
 // leave in place to test if running
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   let data = {
     form: {
       token: process.env.SLACK_AUTH_TOKEN,
-      channel: '#bot',
-      text: 'Hi! I\'m fully functional!',
+      channel: "#bot",
+      text: "Hi! I'm fully functional!",
     },
   };
 
   // post request to slack API
-  request.post('https://slack.com/api/chat.postMessage', data, (error, response, body) => {
+  request.post("https://slack.com/api/chat.postMessage", data, (error, response, body) => {
     res.json(); // sends message
   });
 });
 
 // events
-app.post('/events', (req, res) => {
-      console.log(req.body);
+// TODO: break out in to own file for events
+app.post("/events", (req, res) => {
+  console.log(req.body); // for debugging
 
-      // event verification
-      if (req.body.challenge) {
-        res.send(req.body.challenge);
-        res.end(); // end connection
-      }
+  // event verification
+  if (req.body.challenge) {
+    res.send(req.body.challenge);
+    res.end(); // end connection
+  }
 
-      /**
-       * App Mention (when tacoBytes is called)
-       */
-      if (req.body.event && req.body.event.type === 'app_mention') {
-        /**
-         * Prepare data to be sent via API to Slack
-         * @type {{form: {token: *, link_names: boolean, thread_ts: *, channel: (string|string), text: string}}}
-         */
-        let data = {
-          form: {
-            token: process.env.SLACK_AUTH_TOKEN,
-            link_names: true,
-            thread_ts: req.body.event.event_ts,
-            channel: req.body.event.channel,
-            text: `Hi! <@${req.body.event.user}>!`, // setting default text
-          },
-        };
+  /**
+   * App Mention (when tacoBytes is called)
+   */
+  if (req.body.event && req.body.event.type === "app_mention") {
+    const event = req.body.event;
 
-        // giving tacos - rough
-        if (req.body.event.text && req.body.event.text.indexOf(':taco:') !== -1) {
-          const tacos = req.body.event.text.match(/:taco:/g).length; // number of tacos found
-          let warnings = [];
-          let awarded = [];
-          let users = req.body.event.text.match(/<(.*?)>/g).slice(1); // find all users mentioned in text
+    /**
+     * Prepare data to be sent via API to Slack
+     * @type {{form: {token: *, link_names: boolean, thread_ts: *, channel: (string|string), text: string}}}
+     */
+    let data = {
+      form: {
+        token: process.env.SLACK_AUTH_TOKEN,
+        link_names: true,
+        thread_ts: event.event_ts,
+        channel: event.channel,
+        text: `Hi! <@${event.user}>!`, // setting default text
+      },
+    };
 
-          if (users.length !== 0) { // if at least 2 users are found (bot and 1 user)
+    // Giving tacos - rough
+    if (event.text && areThereTacos(event.text)) {
+      const tacos = getTacos(event.text); // number of tacos in event
+      console.log(tacos);
 
-            users.forEach(user => {
-              let userTacos = 0;
-              // check if user exists in DB
-              client.exists(user, (error, reply) => {
-                console.log(error, reply);
+      const users = findAllUser(event.text); // find all users mentioned in event
 
-                if (reply === 1) {
-                  client.get(user, (err, value) => {
-                    console.log(err, value);
-                    userTacos = parseInt(value) + parseInt(tacos);
-                    client.set(user, parseInt(userTacos), (e, v) => {
+      let messages = [];
 
-                      if (e === null) {
-                        console.log('in awarded');
-                        awarded.push(user + ' has a total of ' + userTacos + '! \n');
-                      }
-                    });
-                  });
-                }
-              });
+      // if at least 2 users are found (bot and 1 user)
+      if (users.length !== 0) {
+
+        // Wrap as promise to make sure all data is ready
+        // when we need it to send back
+        new Promise((resolve, reject) => {
+
+          users.forEach(user => {
+            client.get(user, (err, value) => {
+              if (err) return reject(err); // handle error
+
+              let staleTacos = value; // assign value null | number
+
+              // check if user trying to send self tacos
+              if (event.user && isUserTacoThieving(user, event.user)) {
+
+                // don't update their count, return message and current count
+                messages.push(user + " nice try bad hombre! You still have " + staleTacos + " tacos! \n");
+              } else {
+                // checks if user is found, sets initial count if not,
+                // updates count otherwise
+                staleTacos = setTacos(staleTacos, tacos);
+
+                // update database with number of tacos
+                client.set(user, parseInt(staleTacos)); // update database
+
+                messages.push(user + " now has a total of " + staleTacos + "! \n");
+              }
+
+              resolve();
+
             });
-
-            data.form.text = `${tacos} tacos awarded to ${users.join(', ')} \n ${awarded.join('\n')}`; // add users to text sent as reply
-          }
-
-          request.post('https://slack.com/api/chat.postMessage', data, (error, response, body) => {
-            res.json(); // sends message
-            res.end(); // end connection
           });
-        }
+        })
+          .then(() => {
+            console.log(messages); // for debugging
+
+            // let user know how many tacos were sent
+            data.form.text = `Sending ${tacos} taco(s) to ${users.join(", ")} \n\n`;
+
+            // add all messages to text sent to user
+            data.form.text += `${messages.join("")}`;
+
+            request.post("https://slack.com/api/chat.postMessage", data, (error, response, body) => {
+              res.json(); // sends message
+              res.end(); // end connection
+            });
+          });
+
       }
-    },
-);
+
+    }
+  }
+});
+
+/**
+ * Get all users mentioned except the bot
+ * @param str
+ * @returns Array
+ */
+const findAllUser = str => str.match(/<(.*?)>/g).slice(1);
+
+/**
+ * Get number of tacos
+ * @param str
+ * @returns {number}
+ */
+const getTacos = str => parseInt(str.match(/:taco:/g).length);
+
+/**
+ * Check if even text has tacos
+ * @param str
+ * @returns {boolean}
+ */
+const areThereTacos = str => str.indexOf(":taco:") !== -1;
+
+/**
+ * Check if user is trying to give self tacos
+ * @param users
+ * @param user
+ * @returns {boolean}
+ */
+const isUserTacoThieving = (users, user) => users.indexOf(`<@${user}>`) !== -1;
+
+/**
+ * Check if user key is found.
+ * If not found, set initial taco count
+ * else, update taco count to include fresh new tacos.
+ *
+ * @param staleTacos
+ * @param tacos
+ * @returns {number}
+ */
+const setTacos = (staleTacos, tacos) => staleTacos === null ? tacos : parseInt(staleTacos) + parseInt(tacos);
